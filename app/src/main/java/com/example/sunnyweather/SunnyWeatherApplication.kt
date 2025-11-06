00fcdbf9748d75c62c5790dde48929122c2cc683
chencailing <cailingchen@chinaesport.com>
2025-11-06 14:49:52 +0800

    [feat] 配置_全局令牌值

--- app/src/main/java/com/example/sunnyweather/SunnyWeatherApplication.kt
+++ app/src/main/java/com/example/sunnyweather/SunnyWeatherApplication.kt
@@ -6,6 +6,7 @@ import android.content.Context
 class SunnyWeatherApplication : Application() {
     companion object{
         lateinit var context: Context
+        const val TOKEN = "K1FEY2CJNcOWzGVt"
     }
 
     override fun onCreate(){
